5fb48b5d6ba2cba31a9a58a3954edfed2ef0d29b
pabloaugmatrix <pabloaugmat@gmail.com>
2025-05-05 11:20:59 -0300

    correcao funcao objetivo para o merge

--- src/brute_force/brute_force.py
+++ src/brute_force/brute_force.py
@@ -1,55 +1,59 @@
+import itertools
+from collections import Counter
+from src.entities.warehouse.warehouse import Warehouse
 from src.entities.wave.wave import Wave
-from copy import deepcopy
 
 
-class ObjectiveFunction:
-    def __init__(self, wave: Wave):
-        self.__wave = wave
+def __viable_orders_combination(warehouse: Warehouse, wave: Wave):
+    backlog = warehouse.get_backlog().get_orders()
+    order_combination = []
+    for i in range(1, len(backlog) + 1):
+        order_combination.extend(itertools.combinations(backlog, i))
+    viable_list = []
+    for combination in order_combination:
+        total_len = sum(order.get_length() for order in combination)
+        if wave.get_lower_bound() <= total_len <= wave.get_upper_bound():
+            viable_list.append(combination)
+    return viable_list
 
-    def calculate_objective(self):
-        """
-        Busca quantidade de corredores e pedidos no subconjunto
-        para então calcular a função objetivo(corredores / pedidos).
-        :return: Valor da função objetivo(corredores / pedidos).
-        """
-        # accesses_quantity = self.__wave.get_accesses_quantity()
-        # orders_quantity = self.__wave.get_orders_quantity()
-        # return accesses_quantity / orders_quantity
 
-        accesses_quantity = self.__wave.get_accesses_quantity()
-        orders_quantity = self.__wave.get_itemsMax()
-        return orders_quantity / accesses_quantity
+def __get__better_access_combination(warehouse: Warehouse, wave: Wave, order_combination: list):
+    total_demand = Counter()
+    for order in order_combination:
+        total_demand.update(order.get_itemid_and_quantity_dict())
+    access_combination = []
+    accesses = warehouse.get_accesses()
+    for i in range(1, len(accesses) + 1):
+        access_combination.extend(itertools.combinations(accesses, i))
+    for accesses in access_combination:
+        total_stock = Counter()
+        for access in accesses:
+            total_stock.update(access.get_itemid_and_quantity_dict())
+        if all(total_stock[item] >= qtd for item, qtd in total_demand.items()):
+            return accesses
+    return None
 
-    def capacidadeMax(self, itensDePedidosAtendidos, listOrder, list_ids, qtd, wave: Wave):
-        consumed_items = {}
-        temp_qtd = 0
 
-        for order in listOrder:
-            order_id = order['id']
-            order_items = order['listOrder']
-
-            pedido_atendido = all(
-                (itensDePedidosAtendidos.get(id_pedido, 0) - consumed_items.get(id_pedido, 0)) >= qtd_pedido
-                for id_pedido, qtd_pedido in order_items.items()
-            )
-
-            if pedido_atendido:
-                for id_pedido, qtd_pedido in order_items.items():
-                    consumed_items[id_pedido] = consumed_items.get(id_pedido, 0) + qtd_pedido
-
-                temp_qtd += sum(order_items.values())
-
-                if order_id not in list_ids:
-                    list_ids.append(order_id)
-
-        qtd[0] = temp_qtd
-
-        upper_bound = wave.get_upper_bound()
-        if upper_bound is not None:
-            return temp_qtd <= upper_bound
-
-        lower_bound = wave.get_lower_bound()
-        if lower_bound is not None:
-            return temp_qtd >= lower_bound
-
-        return True
+def brute_force(warehouse: Warehouse, wave: Wave):
+    order_combinations = __viable_orders_combination(warehouse, wave)
+    viable_dict = {}
+    objective = float('inf')
+    for combination in order_combinations:
+        access_combination = __get__better_access_combination(warehouse, wave, combination)
+        if access_combination:
+            viable_dict[combination] = access_combination
+    for orders_tuple, accesses_list in viable_dict.items():
+        total_order_len = sum(order.get_length() for order in orders_tuple)
+        total_access_len = sum(access.get_length() for access in accesses_list)
+        if total_access_len == 0:
+            continue
+        score = total_access_len / total_order_len
+        if score < objective:
+            objective = score
+            wave.empty_orders()
+            wave.empty_visited_accesses()
+            for order in orders_tuple:
+                wave.add_order(order)
+            for access in accesses_list:
+                wave.add_visited_access(access)
+    return wave
--- src/entities/objective_function/objective_function.py
+++ src/entities/objective_function/objective_function.py
@@ -1,4 +1,5 @@
 from src.entities.wave.wave import Wave
+from copy import deepcopy
 
 
 class ObjectiveFunction:
@@ -11,8 +12,44 @@ class ObjectiveFunction:
         para então calcular a função objetivo(corredores / pedidos).
         :return: Valor da função objetivo(corredores / pedidos).
         """
+        # accesses_quantity = self.__wave.get_accesses_quantity()
+        # orders_quantity = self.__wave.get_orders_quantity()
+        # return accesses_quantity / orders_quantity
+
         accesses_quantity = self.__wave.get_accesses_quantity()
-        orders_quantity = self.__wave.get_orders_quantity()
-        if orders_quantity == 0:
-            raise ZeroDivisionError
-        return accesses_quantity / orders_quantity
+        orders_quantity = self.__wave.get_itemsMax()
+        return orders_quantity / accesses_quantity
+
+    def capacidadeMax(self, itensDePedidosAtendidos, listOrder, list_ids, qtd, wave: Wave):
+        consumed_items = {}
+        temp_qtd = 0
+
+        for order in listOrder:
+            order_id = order['id']
+            order_items = order['listOrder']
+
+            pedido_atendido = all(
+                (itensDePedidosAtendidos.get(id_pedido, 0) - consumed_items.get(id_pedido, 0)) >= qtd_pedido
+                for id_pedido, qtd_pedido in order_items.items()
+            )
+
+            if pedido_atendido:
+                for id_pedido, qtd_pedido in order_items.items():
+                    consumed_items[id_pedido] = consumed_items.get(id_pedido, 0) + qtd_pedido
+
+                temp_qtd += sum(order_items.values())
+
+                if order_id not in list_ids:
+                    list_ids.append(order_id)
+
+        qtd[0] = temp_qtd
+
+        upper_bound = wave.get_upper_bound()
+        if upper_bound is not None:
+            return temp_qtd <= upper_bound
+
+        lower_bound = wave.get_lower_bound()
+        if lower_bound is not None:
+            return temp_qtd >= lower_bound
+
+        return True
